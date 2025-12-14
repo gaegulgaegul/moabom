89d3a313519f98c6a548711836a2d8cea3ac01dd
gaegulgaegul <dla9721@gmail.com>
2025-12-15 08:08:54 +0900

    chore(reaction): 허용 이모지 목록 정의
    - ALLOWED_EMOJIS 상수 추가
    - 8개 기본 이모지 정의 (하트, 좋아요, 웃음 등)

--- app/models/reaction.rb
+++ app/models/reaction.rb
@@ -1,6 +1,18 @@
 # frozen_string_literal: true
 
 class Reaction < ApplicationRecord
+  # 허용된 이모지 목록
+  ALLOWED_EMOJIS = %w[
+    ❤️
+    👍
+    😊
+    😍
+    😂
+    🎉
+    👏
+    🔥
+  ].freeze
+
   belongs_to :photo
   belongs_to :user
 
